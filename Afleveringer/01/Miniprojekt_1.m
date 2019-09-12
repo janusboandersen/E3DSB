e6f6116cd7cfc59bfd2470e6f879217a3ef4e2c4
Janus Bo Andersen <janus@janusboandersen.dk>
2019-09-13 07:03:24 +0800

    Initial checkin

---
+++ Afleveringer/01/Miniprojekt_1.m
@@ -0,0 +1,10 @@
+%% E3DSB miniprojekt 1 - Tidsdomæneanalyse
+%%
+% <latex>
+% \Chapter{Indledning}\\
+% Dette miniprojekt behandler tre lydsignaler med analyser i tidsdomænet.
+% Opgaven er løst individuet. Matlab-koden til dette dokument kan findes på
+% 
+% </latex>
+%% E3DSB mini-projekt
+%%% Hello
